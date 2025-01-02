4baf340458170d8f61ff848af0727b6709c243d3
DEB <baruadeb058@gmail.com>
2025-01-02 16:18:59 +0530

    2nd commit adding CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const {db,auth} = require('./firebase')
 const bodyParser = require('body-parser')
@@ -6,6 +7,7 @@ const { route } = require('./routes/authRoute')
 require('dotenv').config()
 
 const port = process.env.PORT || 4010
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
